6873ddeac7913b68e0f094682f095f829dee17d4
BlagovestPanayotov <bobosmile1990@gmail.com>
2024-01-08 18:29:42 +0000

    optimize post list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <TodoList></TodoList>;
+  return (
+    <>
+      <PostList />
+      {/* <TodoList></TodoList> */}
+    </>
+  );
 }
 
 export default App;
