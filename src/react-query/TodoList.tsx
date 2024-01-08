77f6a5d6c439b88ed010c7b6c83a6a30fb653d97
BlagovestPanayotov <bobosmile1990@gmail.com>
2024-01-08 16:55:30 +0000

    handling errors

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -15,12 +15,14 @@ const TodoList = () => {
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
 
-  const { data: todos } = useQuery({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
+  const { data: todos, error } = useQuery(
+    /*<Todo[,Error>*/ {
+      queryKey: ["todos"],
+      queryFn: fetchTodos,
+    }
+  );
 
-  // if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
