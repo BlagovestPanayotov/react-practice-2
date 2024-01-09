67a8da1970035c49b62e86ec066af1e7ff88974a
BlagovestPanayotov <bobosmile1990@gmail.com>
2024-01-09 19:55:49 +0000

    crate custom mutation hook

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { CACHE_KEY_TODOS } from "../constants";
 
 export interface Todo {
   id: number;
@@ -13,13 +14,11 @@ const useTodos = () => {
     axios
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
-  return useQuery(
-    /*<Todo[,Error>*/ {
-      queryKey: ["todos"],
-      queryFn: fetchTodos,
-      staleTime: 10 * 1000,
-    }
-  );
+  return useQuery<Todo[], Error>({
+    queryKey: CACHE_KEY_TODOS,
+    queryFn: fetchTodos,
+    staleTime: 10 * 1000,
+  });
 };
 
 export default useTodos;
