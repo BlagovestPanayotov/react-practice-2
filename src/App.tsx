dcb2ad55c701cdd1b9bbbb953bdf03c909eb53b9
BlagovestPanayotov <bobosmile1990@gmail.com>
2024-01-13 10:06:51 +0000

    create a hook to access context

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import tasksReducer from "./state-management/reducers/tasksReducer";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import TasksContext from "./state-management/contexts/tasksContext";
-import AuthContext from "./state-management/contexts/authContext";
-import authReducer from "./state-management/reducers/loginsStatusReducer";
 import AuthProvider from "./state-management/AuthProvider";
 
 function App() {
